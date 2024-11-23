045c09fd809bfdfd9799b4c62135d97cc014b343
InkLao <eduardo.cornejo.balcazar@gmail.com>
2024-11-23 11:45:13 -0700

    3 capas
    - Persistencia
    - Negocio
    - Presentación

---
+++ bibliotecaMusicalPersistencia/src/main/java/com/mycompany/bibliotecamusicalpersistencia/BibliotecaMusicalPersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bibliotecamusicalpersistencia;
+
+/**
+ *
+ * @author eduar
+ */
+public class BibliotecaMusicalPersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ bibliotecaMusicalPresentacion/src/main/java/com/mycompany/bibliotecamusicalpresentacion/BibliotecaMusicalPresentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bibliotecamusicalpresentacion;
+
+/**
+ *
+ * @author eduar
+ */
+public class BibliotecaMusicalPresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
